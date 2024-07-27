4e337c0c90f063116ce327d6c30ad34b7abde8d5
Excelsio <vapeexcelsio@gmail.com>
2024-07-27 09:25:16 +0300

    add new font, new photo in section together

---
+++ dist/js/script.js
@@ -0,0 +1,177 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // первая карусель
+    const slider1 = tns({
+        container: '.shop__wrapper',
+        items: 4,
+        slideBy: 'page',
+        autoplay: true,
+        gutter: 22,
+        slideBy: 1,
+        controlsContainer: '#custom-control2',
+        autoplayButtonOutput: false,
+        navAsThumbnails: true,
+        // autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                controls: false,
+            },
+            540: {
+                items: 2,
+            },
+            840: {
+                items: 3,
+                controls: true,
+            },
+            1201: {
+                items: 4,
+
+            }
+        }
+    });
+
+    // остановка карусели при наведении мыши
+    const sliderContainer = document.querySelector('.shop__wrapper');
+
+    sliderContainer.addEventListener('mouseover', function () {
+        slider1.pause();
+    });
+    sliderContainer.addEventListener('mouseout', function () {
+        slider1.play();
+    });
+
+    // вторая карусель
+    const slider2 = tns({
+        container: '.shop__wrapper-carousel',
+        items: 4,
+        slideBy: 'page',
+        autoplay: true,
+        gutter: 22,
+        slideBy: 1,
+        controlsContainer: '#custom-control',
+        autoplayButtonOutput: false,
+        navAsThumbnails: true,
+        // autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                controls: false,
+            },
+            540: {
+                items: 2,
+            },
+            840: {
+                items: 3,
+                controls: true,
+            },
+            1201: {
+                items: 4,
+            }
+        }
+    });
+
+        // остановка карусели при наведении мыши
+    const slider2Container = document.querySelector('.shop__wrapper-carousel');
+
+    slider2Container.addEventListener('mouseover', function () {
+        slider2.pause();
+    });
+    slider2Container.addEventListener('mouseout', function () {
+        slider2.play();
+    });
+
+
+    const slider3 = tns({
+        container: '.together__carousel',
+        items: 1,
+        slideBy: 'page',
+        autoplay: true,
+        controls: false,
+        nav: false,
+        autoplayButton: false,
+        autoplayButtonOutput: false,
+
+    });
+
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.header__menu'),
+            menuItem = document.querySelectorAll('.header__link'),
+            hamburger = document.querySelector('.header__hamburger');
+
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('header__hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        });
+
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('header__hamburger_active');
+                menu.classList.toggle('header__menu_active');
+            })
+        })
+    })
+    //modal
+    $("[data-modal=consultation]").on("click", function () {
+        $(".overlay, #consultation").fadeIn();
+    });
+    $(".modal__close").on("click", function () {
+        $(".overlay, #consultation, #thanks").fadeOut();
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2,
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!"),
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты",
+                },
+            },
+        });
+    }
+
+    validateForms("#consultation form");
+
+    $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize(),
+        }).done(function () {
+            $(this).find("input").val("");
+            $("#consultation").fadeOut();
+            $(".overlay, #thanks").fadeIn("slow");
+
+            $("form").trigger("reset");
+        });
+        return false;
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $(".pageup").fadeIn();
+        } else {
+            $(".pageup").fadeOut();
+        }
+    });
+    new WOW().init();
+})
+
--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,13 @@ const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "./src"
+            baseDir: "dist"
         }
     });
 
@@ -18,7 +19,7 @@ gulp.task('server', function() {
 
 // задача аля watch sass
 gulp.task('styles', function () {
-    return gulp.src('src/sass/**/*.sass')
+    return gulp.src('src/sass/**/*.+(scss|sass)')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename({
         prefix: "",
@@ -28,16 +29,53 @@ gulp.task('styles', function () {
 			cascade: false
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 })
 
-// при изменнии файлов sass,html будет запускаться эта задача
-gulp.task('watch', function () {
-    // sass
-    gulp.watch('src/sass/**/*.sass', gulp.parallel('styles'));
-    // html
-    gulp.watch('src/*.html').on('change', browserSync.reload);
-})
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task('mailer', function () {
+    return gulp.src("src/mailer/**/*")
+        .pipe(gulp.dest("dist/mailer"));
+});
+
+gulp.task('images', function () {
+    return gulp.src("src/img/**/*")
+        .pipe(gulp.dest("dist/img"));
+});
+
+
+// // при изменнии файлов sass,html будет запускаться эта задача
+// gulp.task('watch', function () {
+//     // sass
+//     gulp.watch('src/sass/**/*.sass', gulp.parallel('styles'));
+//     // html
+//     gulp.watch('src/*.html').on('change', browserSync.reload);
+// })
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'mailer', 'html', 'images'));
